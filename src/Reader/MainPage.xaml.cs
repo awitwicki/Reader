8653a6d81f893bc55dc6debecfbc5ad140969f71
Andrew Witwicki <vitvitskiy3@gmail.com>
2023-02-20 21:21:29 +0100

    Initial project commit

---
+++ src/Reader/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Reader;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Reader/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Reader;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
